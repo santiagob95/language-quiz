bd88ef392c93a161e6a1bc808003875a37e3a9ab
Meuge <euge672@gmail.com>
2020-05-08 13:37:36 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# language-quiz
+### Aplicación de Lengua en forma de Quiz dirigida para chicos en el primario
+
+Para correrlo en su dispositivo debe contar con [Node.js](https://nodejs.org/en/download/)
+
+Luego para ejecutar el proyecto y visitarlo en su localhost, debe correr los siguientes comandos
+
+#### `npm install`
+
+#### `npm start`
+
+También puede visitarlo en  https://language-quiz--meuge.repl.co
+
+_____________________________________________
+
+
+### Language Quiz App for primary school children
+
+To run this project you need to have [Node.js](https://nodejs.org/en/download/) installed in your machine 
+
+In order to be able to run it, please follow the next commands:
+
+#### `npm install`
+
+#### `npm start`
+
+You can also check it in https://language-quiz--meuge.repl.co
+
+
+
+[![ForTheBadge built-with-love](http://ForTheBadge.com/images/badges/built-with-love.svg)](http://ForTheBadge.com/)
+[![ForTheBadge winter-is-coming](http://ForTheBadge.com/images/badges/winter-is-coming.svg)](http://ForTheBadge.com)
---
+++ src/questions/allQuestions.js
@@ -0,0 +1,92 @@
+var quizQuestions = [
+  {
+      question: "Completa la siguiente frase:«No ______ más café.»",
+      answers: [
+          {
+              result:"incorrect",
+              content: "ahí",
+             
+          },
+          {
+              result:"incorrect",
+              content: "ay",
+              
+          },
+          {
+              result:"correct",
+              content: "hay",
+              
+          }
+      ]
+  },
+  {
+      question: "Completa la siguiente frase:«Me comería una bolsa de _____»",
+      answers: [
+          {
+              result:"incorrect",
+              content: "manises"
+          },
+          {
+              result:"correct",
+              content: "maníes"
+          },
+          {
+              result:"incorrect",
+              content: "manís"
+          }
+      ]
+  },
+  {
+      question: "Completa la siguiente frase:«¡______ si nos juntamos algún día!»",
+      answers: [
+          {
+              result:"incorrect",
+              content: "Haber"
+          },
+          {
+              result:"incorrect",
+              content: "Haver"
+          },
+          {
+              result:"correct",
+              content: "A ver"
+          }
+      ]
+  },
+  {
+      question: "Completa la siguiente frase: «No debes perder la _____»",
+      answers: [
+          {
+              result:"incorrect",
+              content: "concentrasión"
+          },
+          {
+              result:"correct",
+              content: "concentración"
+          },
+          {
+              result:"incorrect",
+              content: "consentración"
+          }
+      ]
+  },
+  {
+      question: "El sufijo de la palabra pasear es:",
+      answers: [
+          {
+              result:"incorrect",
+              content: "-pas"
+          },
+          {
+              result:"incorrect",
+              content: "-ar"
+          },
+          {
+              result:"correct",
+              content: "ear"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
